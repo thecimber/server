666745e8f1e85f7b360b3cf149cdccc5106e6a79
The cimber <thecimber98@gmail.com>
2025-02-22 10:42:08 -0800

    Update games

--- src/constants.js
+++ src/constants.js
@@ -5,3 +5,28 @@ export const TELEGRAM_CHANEL = ''
 export const PATH_SERVER = 'https://raw.githubusercontent.com/thecimber/server/refs/heads/main'
 
 export const PATH_SERVER_PS3 = 'https://raw.githubusercontent.com/thecimber/server/refs/heads/main/ps3/'
+
+
+export const PROPS_MEDIFIRE = {
+    name: "Mediafire",
+    icon: "fa-solid fa-download", 
+    color: "btn-primary"
+}
+
+export const PROPS_TERABOX= {
+    name: "Terabox",
+    icon: "fas fa-box",
+    color: "btn-warning"
+}
+
+export const PROPS_REPUESTO= {
+    name: "Repuesto",
+    icon: "fas fa-file-download",
+    color: "btn-success"
+}
+
+export const PROPS_ACTUALIZACION= {
+    
+}
+
+
--- src/data/ps3-games.js
+++ src/data/ps3-games.js
@@ -1,40 +1,118 @@
-import { DOWNLOAD_TUTORIAL } from "../constants";
+import { DOWNLOAD_TUTORIAL, PROPS_ACTUALIZACION, PROPS_MEDIFIRE, PROPS_TERABOX } from "../constants";
 
 export const gamesPs3 =
     [
         {
-            id: 1,
-            name: 'Sonic the Hedgehog',
+            id: 949494,
+            name: 'dbz',
             image: 'sonic-the-hedgehog-ps3-300x350.webp',
-            format: ['iso', 'pkg'],
-            language: ['Español'],
-            genre: ['Acción', 'Aventura', 'Plataforma'],
-            size: '7.56 GB',
-            downloadFormat: ['mediafire', 'Gofile'],
+            format: ["pkg", "iso"],
+            language: ["Español", "Ingles"],
+            genre: ["Acción", "aventura"],
+            size: '122 mb',
+            link: '',
+            downloadTutorial: DOWNLOAD_TUTORIAL,
+            date: '2025-02-12',
+            downloadMethods: [
+                { ...PROPS_MEDIFIRE, link: "htpps://" }
+            ]
+        },
+        {
+            id: 576867,
+            name: 'Minecraft PlayStation 3 Edition',
+            image: 'Minecraft-PlayStation-3-Edition.webp',
+            format: ["ISO/PKG"],
+            language: ["Español"],
+            genre: ["Acción", "aventuras", "construcción"],
+            size: '238mb',
             link: '',
             downloadTutorial: DOWNLOAD_TUTORIAL,
             date: '',
             downloadMethods: [
-                {
-                    name: "Mediafire",
-                    icon: "fa-solid fa-download", 
-                    color: "btn-primary",
-                    link: "https://www.mediafire.com/example"
-                },
-                {
-                    name: "Terabox",
-                    icon: "fas fa-box",
-                    color: "btn-warning",
-                    link: "https://www.terabox.com/example"
-                },
-                {
-                    name: "Repuesto",
-                    icon: "fas fa-file-download",
-                    color: "btn-success",
-                    link: "https://www.gofile.io/example"
-                }
+                { ...PROPS_MEDIFIRE, link: "https://cuty.io/Kjk47DVXv7" },
+                { ...PROPS_TERABOX, link: "https://1024terabox.com/s/15Ru-qoPs8sTZebGOux0E6Q" }
             ]
         },
-        
+        {
+            id: 431095,
+            name: 'DuckTales: Remasterizado',
+            image: 'DuckTales-Remastered-ps3-300x355.jpg',
+            format: ["ISO/PKG"],
+            language: ["Español"],
+            genre: ["Acción", "Plataformas"],
+            size: '670MB',
+            link: '',
+            downloadTutorial: DOWNLOAD_TUTORIAL,
+            date: '',
+            downloadMethods: [
+                { ...PROPS_MEDIFIRE, link: "https://cuty.io/d8IXWYcjDI" },
+                { ...PROPS_TERABOX, link: "https://1024terabox.com/s/16TFdRP-SS6x6Vq9lGLTTLQ" }
+            ]
+        },
+        {
+            id: 948741,
+            name: 'Need for Speed: Most Wanted',
+            image: 'Most-Wanted-ps3.webp',
+            format: ["ISO/PKG"],
+            language: ["Español"],
+            genre: ["Racing"],
+            size: '4.39 GB',
+            link: '',
+            downloadTutorial: DOWNLOAD_TUTORIAL,
+            date: '',
+            downloadMethods: [
+                { ...PROPS_MEDIFIRE, link: "https://cuty.io/95QvVXT0" },
+                { ...PROPS_TERABOX, link: "https://1024terabox.com/s/1D-A5-hG6v2XMmKUdpL7G5w" }
+            ]
+        },
+        {
+            id: 886589,
+            name: 'Sonic the Hedgehog',
+            image: 'sonic-the-hedgehog-ps3-300x350.webp',
+            format: ["ISO/PKG"],
+            language: ["Español"],
+            genre: ["Action", "Adventure", "Platformer"],
+            size: '7.80 GB',
+            link: '',
+            downloadTutorial: DOWNLOAD_TUTORIAL,
+            date: '',
+            downloadMethods: [
+                { ...PROPS_MEDIFIRE, link: "https://cuty.io/R6KbjpkSjRm" },
+                { ...PROPS_TERABOX, link: "https://1024terabox.com/s/1oLPTbjewvxvwJlufkbsBbA" }
+            ]
+        },
+        {
+            id: 601510,
+            name: 'Dragon Ball Xenoverse',
+            image: 'dragon-ball-xenoverse-ps3.webp',
+            format: ["ISO/PKG"],
+            language: ["Español"],
+            genre: ["Lucha", "juego de rol"],
+            size: '6,23 GB',
+            link: '',
+            downloadTutorial: DOWNLOAD_TUTORIAL,
+            date: '',
+            downloadMethods: [
+                { ...PROPS_MEDIFIRE, link: "https://cuty.io/ugz3YVe" },
+                { ...PROPS_TERABOX, link: "https://1024terabox.com/s/1DV6BVIeZeKPQfEG4Kp3QIQ" }
+            ]
+        },
+        {
+            id: 575759,
+            name: '  Dragon Ball: Raging Blast 2',
+            image: 'Dragon-Ball-Raging-Blast-2-ps3.webp',
+            format: ["ISO/PKG"],
+            language: ["Español"],
+            genre: ["Fighting"],
+            size: '5,94 GB',
+            link: '',
+            downloadTutorial: DOWNLOAD_TUTORIAL,
+            date: '',
+            downloadMethods: [
+                { ...PROPS_MEDIFIRE, link: "https://cuty.io/FcKcMxF" },
+                { ...PROPS_TERABOX, link: "https://1024terabox.com/s/1jZrlwOl701eqPJqGVerfZw" }
+            ]
+        }
+
 
     ]
