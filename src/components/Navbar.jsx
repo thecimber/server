fd474708f8062838c9a67da12652d84bacc0908b
The cimber <thecimber98@gmail.com>
2025-02-22 13:29:20 -0800

    ajuste logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom"
+import logo from '../assets/logo.jpg';
 
 export const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <img className="logo-navbar" src={logo} alt="" />
             <a className="navbar-brand" href="#" translate="no">TheCimberRomsYT</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
