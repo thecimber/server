3df99779983b35ba171559bdb7bc138fa59918f7
The cimber <thecimber98@gmail.com>
2025-02-25 22:10:35 -0800

    Mnejar rutas que no existen

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Navbar } from './components/Navbar'
 import { Home } from './pages/Home'
 import { EmulatorDetails } from './pages/EmulatorDetails'
 import { HeroSection } from './components/HeroSection'
-import { Route, Router, Routes } from 'react-router-dom'
+import { Navigate, Route, Router, Routes } from 'react-router-dom'
 import { Dmca } from './components/Dmca'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
@@ -21,6 +21,7 @@ function App() {
         <Route path="/emulador/:emulatorGame" element={<EmulatorDetails />} />
         <Route path="/games" element={<GamesSearch />} />
         <Route path="/dmca" element={<Dmca />} />
+        <Route path="*" element={<Navigate to="/" />} />
 
       </Routes>
     </>
