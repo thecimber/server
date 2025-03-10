095156fa5c735f409b33ffd71669bd10602b7118
thecimber <thecimber98@gmail.com>
2025-03-10 00:25:41 +0000

    Actualización de juegos de psp

--- src/data/psp-games.js
+++ src/data/psp-games.js
@@ -1,5 +1,17 @@
 import { DOWNLOAD_TUTORIAL, PROPS_ACTUALIZACION, PROPS_MEDIFIRE, PROPS_TERABOX } from "../constants";
 
-export const gamesPsp = [
-
-];
+export const gamesPsp = [{
+                id: "A41iI9ad",
+                name: "Assassin's Creed Bloodlines",
+                image: "undefinedAssassins-Creed-Bloodlines.jpg",
+                format: ["ISO"],
+                language: ["Español","Inglés"],
+                genre: ["Acción","Aventuras","Fighting"],
+                size: "554.1 MB",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/16EOcwETGDj-fV5WC1reV2Q" }
+                ]
+            }];
