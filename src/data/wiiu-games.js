71ec337aa563909593114ee4339ebcdc74b354fd
thecimber <thecimber98@gmail.com>
2025-03-11 19:29:24 +0000

    Actualización de juegos de wiiu

--- src/data/wiiu-games.js
+++ src/data/wiiu-games.js
@@ -1,5 +1,17 @@
 import { DOWNLOAD_TUTORIAL, PROPS_ACTUALIZACION, PROPS_MEDIFIRE, PROPS_TERABOX } from "../constants";
 
-export const gamesWiiu = [
-    
-];
+export const gamesWiiu = [{
+                id: "tynMFupk",
+                name: "007 LEGENDS",
+                image: "007-LEGENDS.jpg",
+                format: ["WUA"],
+                language: ["Multi-Idioma"],
+                genre: ["Acción"],
+                size: "8.9 Gb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_MEDIFIRE , link: "https://cuty.io/ek1dtTVR" },{ ...PROPS_TERABOX , link: "https://1024terabox.com/s/1XOqUsTwAUbaDBxoLkM0sKg" }
+                ]
+            }];
