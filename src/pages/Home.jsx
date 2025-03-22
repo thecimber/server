321fa141edac48fb2761e9b26832aad0547fb435
thecimber <thecimber98@gmail.com>
2025-03-21 22:04:01 -0700

    Notificaciones

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,39 @@
+import { ToastContainer, toast } from 'react-toastify';
 import { EmulatorCard } from "../components/EmulatorCard";
 import { getEmulators } from "../services/emulatorService";
 import '../styles/home.css';
+import { useEffect } from "react";
 
 export const Home = () => {
     const emulators = getEmulators();
+    const notify = () => toast.success("Nuevo contenido para PC disponible!");
+
+    useEffect(() => {
+        notify();
+    }, []);
+
     return (
-        <div className="home">
-            <h1 className="text-white">Emuladores</h1>
-            <div className="emuladores-list">
-                {emulators.map((emulator) => (
-                    <EmulatorCard key={emulator.id} emulator={emulator} />
-                ))}
+        <>
+            <div className="home">
+                <h1 className="text-white">Emuladores</h1>
+                <div className="emuladores-list">
+                    {emulators.map((emulator) => (
+                        <EmulatorCard key={emulator.id} emulator={emulator} />
+                    ))}
+                </div>
             </div>
-        </div>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick={false}
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
+            />
+        </>
     )
 }
