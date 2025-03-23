06547cb798fc0fbc699fbe32b6a3d41c946e5d37
thecimber <thecimber98@gmail.com>
2025-03-22 20:46:23 -0700

    Botón para explorar más contenido

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -20,7 +20,7 @@ export const HeroSection = () => {
     }
     return (
         <div className="hero-section">
-            <div className="hero-content container">
+            <div className="hero-content">
                 <h1 className='title-hero-section'>Bienvenido a nuestra plataforma</h1>
                 <p className='description-hero-section'>Descubre los mejores juegos para tus emuladores favoritos.</p>
                 <SearchForm search={startSearch} handleSubmit={handleSubmit} handleChange={handleChange}></SearchForm>
