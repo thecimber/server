3de47945a24dcf55fac68d0e0b3757a40a753199
thecimber <thecimber98@gmail.com>
2025-03-23 13:35:32 -0700

    Solicitar juegos

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -24,7 +24,10 @@ export const HeroSection = () => {
                 <h1 className='title-hero-section'>Bienvenido a nuestra plataforma</h1>
                 <p className='description-hero-section'>Descubre los mejores juegos para tus emuladores favoritos.</p>
                 <SearchForm search={startSearch} handleSubmit={handleSubmit} handleChange={handleChange}></SearchForm>
-                <a className='btn-animated btn-animated-blue w-20 hide-button' href='https://www.youtube.com/@thecimber' target="_blank" >Explorar más contenido</a>
+                <div className="container-btn-hero-section">
+                    <a className='btn-animated btn-animated-blue w-20 hide-button mx-2' href='https://www.youtube.com/@thecimber' target="_blank" >Explorar más contenido</a>
+                    <a className='btn-animated btn-animated-green w-20 hide-button mx-2' href='https://docs.google.com/forms/d/e/1FAIpQLSf4Ug4sYuRQ888QbA-K7p6YI5x_cIQgbiB6xvR-ZlFi0mtWog/viewform?usp=header' target="_blank" >Solicitar Juegos</a>
+                </div>
             </div>
         </div>
     )
