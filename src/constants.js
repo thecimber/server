64671519c547aedda41fd704ae3034d2c6df3380
thecimber <thecimber98@gmail.com>
2025-03-24 14:11:51 -0700

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -18,7 +18,7 @@ export const PATH_SERVER_GAMECUBE = 'https://raw.githubusercontent.com/thecimber
 
 export const PATH_SERVER_PS2 = 'https://raw.githubusercontent.com/thecimber/server/refs/heads/main/ps2/';
 
-export const PATH_SERVER_SW = 'https://raw.githubusercontent.com/thecimber/server/refs/heads/main/nw/';
+export const PATH_SERVER_SW = 'https://raw.githubusercontent.com/thecimber/server/refs/heads/main/sw/';
 
 
 export const PROPS_MEDIFIRE = {
