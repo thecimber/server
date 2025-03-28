adedb247bd6298cb592610923aed1cbea6cfc048
thecimber <thecimber98@gmail.com>
2025-03-27 18:54:10 -0700

    update games psp

--- src/data/psp-games.js
+++ src/data/psp-games.js
@@ -14,4 +14,466 @@ export const gamesPsp = [{
                 downloadMethods: [
                     { ...PROPS_TERABOX , link: "https://1024terabox.com/s/16EOcwETGDj-fV5WC1reV2Q" }
                 ]
+            },{
+                id: "48XBURhK",
+                name: "UEFA Euro 2008",
+                image: "UEFA Euro 2008.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Deportes"],
+                size: "1 Gb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1cg6uVLte4miY0JKYQlc02w" }
+                ]
+            },{
+                id: "78tMI0F6",
+                name: "UEFA champions league 2006-2007",
+                image: "UEFA champions league 2006-2007.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Deportes"],
+                size: "undefined",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1Qkbs_7qP_ohxiEOk88VSyA" }
+                ]
+            },{
+                id: "sFrvmBl0",
+                name: "Pes Pro evolution Soccer 6",
+                image: "Pes Pro evolution Soccer 6.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Deportes"],
+                size: "849 Mb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1zldHiV27PWRBgTuo9QCtxA" }
+                ]
+            },{
+                id: "rsfUDaGP",
+                name: "FIFA World Cup Germany 2006",
+                image: "FIFA World Cup Germany 2006.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Deportes"],
+                size: "700 Mb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1oGgJRDe2rrMltRzsra8hAw" }
+                ]
+            },{
+                id: "kILiuJ0t",
+                name: "FIFA Street 2",
+                image: "FIFA Street 2.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Deportes"],
+                size: "218 Mb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1gC9TFF4S9llP7RE5X46mhA" }
+                ]
+            },{
+                id: "lh14BHks",
+                name: "FIFA 12",
+                image: "FIFA 12.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Deportes"],
+                size: "828.2 Mb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1ZI8CP-_h8lKJ2yoym-1DHQ" }
+                ]
+            },{
+                id: "ybYHaBCB",
+                name: "FIFA 11",
+                image: "FIFA 11.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Deportes"],
+                size: "826 Mb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1XW9suWUjszDDei9jHAooxA" }
+                ]
+            },{
+                id: "BE6QPbHC",
+                name: "FIFA 10",
+                image: "FIFA 10.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Deportes"],
+                size: "819 Mb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1DBLc9pBWHMVDeg3Z3sI84g" }
+                ]
+            },{
+                id: "C2Dwb4YU",
+                name: "FIFA 09",
+                image: "FIFA 09.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Deportes"],
+                size: "809 Mb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1dTMQPNni1T2dvJVv_vxcig" }
+                ]
+            },{
+                id: "48eMVtVD",
+                name: "FIFA 08",
+                image: "FIFA 08.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Deportes"],
+                size: "848 Mb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1-Ih6b5vNUFjyN8IYOxsUkQ" }
+                ]
+            },{
+                id: "ew__Qvkl",
+                name: "FIFA 07",
+                image: "FIFA 07.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Deportes"],
+                size: "838 Mb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1GsZQmd5xvcyaWTE0cHOksQ" }
+                ]
+            },{
+                id: "MKR58pKB",
+                name: "FIFA 06",
+                image: "FIFA 06.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Deportes"],
+                size: "644 Mb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1zs4j05JiYaxUiTIgAeIlvA" }
+                ]
+            },{
+                id: "vlHB-X0u",
+                name: "2010 FIFA World Cup South Africa ",
+                image: "2010 FIFA World Cup South Africa.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Deportes"],
+                size: "859 Mb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1v0ByhwB5-gZxXzhQMFnNsg" }
+                ]
+            },{
+                id: "fErxdplC",
+                name: "Star Wars - Battlefront II ",
+                image: "Star Wars - Battlefront II.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Acción"],
+                size: "1.13 Gb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1FQ9qCK4fY968sOETEiqP-w" }
+                ]
+            },{
+                id: "a-dM1lLl",
+                name: "Star Wars Battlefront - Renegade Squadron ",
+                image: "Star Wars Battlefront - Renegade Squadron.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Acción"],
+                size: "1.1 Gb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1tMcNSSV0rTYvPpTtHjVPbQ" }
+                ]
+            },{
+                id: "KsN97421",
+                name: "Star Wars - The Clone Wars - Republic Heroes",
+                image: "Star Wars - The Clone Wars - Republic Heroes.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Acción"],
+                size: "1.0 Gb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1pVIwkIEGlmPL-1LfHw8LSw" }
+                ]
+            },{
+                id: "vJwKhHKL",
+                name: "Star Wars - Lethal Alliance ",
+                image: "Star Wars - Lethal Alliance.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Acción"],
+                size: "730 Mb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1qVAXJoJ0aTXiErx6T4norw" }
+                ]
+            },{
+                id: "ampVnULe",
+                name: "Star Wars Battlefront - Elite Squadron ",
+                image: "Star Wars Battlefront - Elite Squadron.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Acción"],
+                size: "1.2 Gb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1vXynjl2FW0C2HwltB_lMMQ" }
+                ]
+            },{
+                id: "DfNQ7PgO",
+                name: "Star Wars - The Force Unleashed",
+                image: "Star Wars - The Force Unleashed.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Acción"],
+                size: "829 Mb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1sBrVONFbRXQSEAzo21UfSw" }
+                ]
+            },{
+                id: "jWTUQVm_",
+                name: "Sonic Rivals",
+                image: "Sonic Rivals.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Aventura"],
+                size: "113 Mb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1yXv7NXlbxgBJghv1EuqVrg" }
+                ]
+            },{
+                id: "vo1hM5Gr",
+                name: "Sonic Rivals 2",
+                image: "Sonic Rivals 2.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Aventura"],
+                size: "240 Mb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1AeRfCRvvhlVVuF8jGa-8qA" }
+                ]
+            },{
+                id: "B3pkOKu5",
+                name: "LEGO Star Wars 3 the clone wars",
+                image: "LEGO Star Wars 3 the clone wars.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Acción"],
+                size: "1.4 Gb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1_DjjZyz6Pdo6Ouj-gTmi0Q" }
+                ]
+            },{
+                id: "HTHKzc1z",
+                name: "LEGO Star Wars The Original Trilogy ",
+                image: "LEGO Star Wars The Original Trilogy.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Acción"],
+                size: "876 Mb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1zy6HPw1nGi20hftT-4LHZQ" }
+                ]
+            },{
+                id: "ZrJsLPPs",
+                name: "LEGO Pirates of the Caribbean - The Video Game",
+                image: "LEGO Pirates of the Caribbean - The Video Game.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Acción"],
+                size: "1.2 Gb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/12xcaLbg1xSRlvjGpD6Jlug" }
+                ]
+            },{
+                id: "hmMW9S0L",
+                name: "LEGO Indiana Jones 2 - The Adventure Continues",
+                image: "LEGO Indiana Jones 2 - The Adventure Continues.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Acción"],
+                size: "506.01 Mb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1254ec3sWpsS1U0nHzVkndg" }
+                ]
+            },{
+                id: "Da7azxqM",
+                name: "LEGO Indiana Jones - The Original Adventures",
+                image: "LEGO Indiana Jones - The Original Adventures.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Acción"],
+                size: "669.1 Mb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1y-on10Vf9zOkf0ZWkesmKw" }
+                ]
+            },{
+                id: "3eqAc0kW",
+                name: "LEGO Harry Potter - Years 1-4",
+                image: "LEGO Harry Potter - Years 1-4.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Acción"],
+                size: "1.2 Gb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1vVH9fY9y3j_UGaz8BIJ3Pg" }
+                ]
+            },{
+                id: "r3IDxmQg",
+                name: "LEGO Harry Potter - Years 5-7",
+                image: "LEGO Harry Potter - Years 5-7.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Acción"],
+                size: "1.3 Gb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1HNoZtKjOF2R9dkV73RPY-A" }
+                ]
+            },{
+                id: "mOFjTN9-",
+                name: "LEGO Batman - The Video Game",
+                image: "LEGO Batman - The Video Game.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Acción"],
+                size: "591 Mb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1LFbUadc96PCIVHiUt5liGw" }
+                ]
+            },{
+                id: "JvP5H6-3",
+                name: "God of War - Ghost of Sparta",
+                image: "God of War - Ghost of Sparta.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Acción"],
+                size: "1.2 Gb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1Ufs03Pxl0z4ct1hWu3wHJw" }
+                ]
+            },{
+                id: "HD_FXHQv",
+                name: "God of War - Chains of Olympus",
+                image: "God of War - Chains of Olympus.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Acción"],
+                size: "1.2 GB",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1mUnmMQIobmUZQbicIP93sA" }
+                ]
+            },{
+                id: "Sf2teo6A",
+                name: "Crash Tag Team Racing",
+                image: "Crash Tag Team Racing.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Aventura"],
+                size: "959 Mb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/18-pEtOwpJn5EqfwTrybpxA" }
+                ]
+            },{
+                id: "iqB6q8ko",
+                name: "Crash of the Titans",
+                image: "Crash of the Titans.jpeg",
+                format: ["ISO"],
+                language: ["Multi-Idioma"],
+                genre: ["Aventura"],
+                size: "819 Mb",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1jj4oJ-1WgfRU0NyEyxljbQ" }
+                ]
             }];
