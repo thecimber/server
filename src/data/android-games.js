e3261e52b06eb6a2ae505cc17b41fbe05edb8e29
thecimber <thecimber98@gmail.com>
2025-03-31 21:41:01 -0700

    update games android

--- src/data/android-games.js
+++ src/data/android-games.js
@@ -14,4 +14,102 @@ export const gamesAndroid = [{
                 downloadMethods: [
                     { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1EktatH2Fg0sLGR93H3M0SA" }
                 ]
+            },{
+                id: "_Sw8suwF",
+                name: "Five Nights At Freddy's v2.0.6",
+                image: "Five Nights At Freddys v2.0.6.jpeg",
+                format: ["APK"],
+                language: ["Multi-Idioma"],
+                genre: ["Terror","Suspenso"],
+                size: "154 Mb [8.1 Mb]",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1jAis7orroWZgZKaIWsy2Vw" }
+                ]
+            },{
+                id: "P85PNZuh",
+                name: "Five Nights At Freddy's 2 v2.0.6",
+                image: "Five Nights At Freddys 2 v2.0.6.jpeg",
+                format: ["APK"],
+                language: ["Multi-Idioma"],
+                genre: ["Terror","Suspenso"],
+                size: "115 Mb [8,1 Mb]",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1TffdCq8LXqgudkcHkGhCWQ" }
+                ]
+            },{
+                id: "zOuiR3eo",
+                name: "Five Nights At Freddy's 3 v2.0.3",
+                image: "Five Nights At Freddys 3 v2.0.3.jpeg",
+                format: ["APK"],
+                language: ["Multi-Idioma"],
+                genre: ["Terror","Suspenso"],
+                size: "105 Mb [8,1 Mb]",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1xnQRmNdB4eYbe8qiejFXrw" }
+                ]
+            },{
+                id: "gI2Bc7IZ",
+                name: "Five Nights At Freddy's 4 v2.0.3",
+                image: "Five Nights At Freddys 4 v2.0.3.jpeg",
+                format: ["APK"],
+                language: ["Multi-Idioma"],
+                genre: ["Terror","Suspenso"],
+                size: "55,6 Mb [8,1 Mb]",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1rSrEp9k-byh01bzHC3nDtQ" }
+                ]
+            },{
+                id: "bwUQysgR",
+                name: "Five Nights At Freddy's: sister location v2.0.4",
+                image: "Five Nights At Freddy's: sister location v2.0.4.jpeg",
+                format: ["APK"],
+                language: ["Multi-Idioma"],
+                genre: ["Terror","Suspenso"],
+                size: "296MB [8,1MB]",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1x0lS_9_VY8SI9ACyUxyFpw" }
+                ]
+            },{
+                id: "B2abiMQo",
+                name: "Blasphemous v1.2.0",
+                image: "Blasphemous v1.2.0.jpeg",
+                format: ["APK"],
+                language: ["Multi-Idioma"],
+                genre: ["Suspenso","Acción","2D"],
+                size: "905 Mb [8,1 Mb]",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1-PT9kk4Yvi3zWZcq6NHkIw" }
+                ]
+            },{
+                id: "dkVSYFub",
+                name: "Cuphead v1.50",
+                image: "Cuphead v1.50.jpeg",
+                format: ["APK"],
+                language: ["Multi-Idioma"],
+                genre: ["Acción","Aventuras","2D"],
+                size: "1,7 Gb [8,1 Mb]",
+                link: "",
+                downloadTutorial: DOWNLOAD_TUTORIAL,
+                date: "",
+                downloadMethods: [
+                    { ...PROPS_TERABOX , link: "https://1024terabox.com/s/1ISLbV7CeMdlsGe_kcrkGNg" }
+                ]
             }];
